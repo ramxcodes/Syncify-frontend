5dd3e68c8f6329b5cc54a6cfeee4b31bbd7cf463
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-16 04:03:56 +0530

    Added MainLayout and chat page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/ss0-callback" element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}/>
+        <Route
+          path="/ss0-callback"
+          element={
+            <AuthenticateWithRedirectCallback
+              signUpForceRedirectUrl={"/auth-callback"}
+            />
+          }
+        />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Route>
       </Routes>
     </>
   );
