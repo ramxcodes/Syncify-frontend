564f8a03c5dbbc7c3fd6235502612154707fbed9
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-16 23:37:47 +0530

    Changed Styling 💅🏻

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,32 +1,40 @@
-import { SignedIn, SignedOut, SignOutButton } from "@clerk/clerk-react"
-import { LayoutDashboardIcon } from "lucide-react"
-import { Link } from "react-router-dom"
-import SIngInOAuthButtons from "./SIngInOAuthButtons"
-
-const isAdmin = false
+import { SignedOut, UserButton } from "@clerk/clerk-react";
+import { LayoutDashboardIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+import SIngInOAuthButtons from "./SIngInOAuthButtons";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "./ui/button";
 
 const Topbar = () => {
+  const { isAdmin } = useAuthStore();
+  console.log({ isAdmin });
+
   return (
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
-        <div className="flex gap-2 items-center">
-            Syncify
-        </div>
-        <div className="flex items-center gap-4">
-            {isAdmin && (
-                <Link to={"/admin"}>
-                    <LayoutDashboardIcon className="size-4 mr-2" />
-                    Admin Dashboard
-                </Link>
-            )}
-            <SignedIn>
-                <SignOutButton />
-            </SignedIn>
-            <SignedOut>
-                <SIngInOAuthButtons />
-            </SignedOut>
-        </div>
-    </div>
-  )
-}
+      <div className="flex gap-2 items-center">
+        <img src="logo.png" alt="Syncify logo" className="w-28" />
+        {/* Syncify */}
+      </div>
+      <div className="flex items-center gap-4">
+        {isAdmin && (
+          <Link
+            to={"/admin"}
+            className={cn(buttonVariants({ variant: "outline" }))}
+          >
+            <LayoutDashboardIcon className="size-4 mr-2" />
+            Admin Dashboard
+          </Link>
+        )}
 
-export default Topbar
+        <SignedOut>
+          <SIngInOAuthButtons />
+        </SignedOut>
+
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Topbar;
