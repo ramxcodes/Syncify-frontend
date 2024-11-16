2e466a89cec94f7c4541a2c36f425e8f7f63c3b8
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-17 04:10:41 +0530

    Added Play button in Featured section ▶️

--- src/pages/home/components/FeaturedSection.tsx
+++ src/pages/home/components/FeaturedSection.tsx
@@ -1,8 +1,10 @@
-import FeaturedGridSkeleton from "@/components/skeletons/FeaturedGridSkeleton";
 import { useMusicStore } from "@/stores/useMusicStore";
+import FeaturedGridSkeleton from "@/components/skeletons/FeaturedGridSkeleton";
+import PlayButton from "./PlayButton";
 
 const FeaturedSection = () => {
   const { isLoading, featuredSongs, error } = useMusicStore();
+
   if (isLoading) return <FeaturedGridSkeleton />;
 
   if (error)
@@ -11,9 +13,8 @@ const FeaturedSection = () => {
         {error} Something went wrong
       </div>
     );
-
   return (
-    <div className="grid grid-col-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
       {featuredSongs.map((song) => (
         <div
           key={song._id}
@@ -26,14 +27,13 @@ const FeaturedSection = () => {
             className="w-16 sm:w-20 h-16 sm:h-20 object-cover flex-shrink-0"
           />
           <div className="flex-1 p-4">
-                <div className="font-medium truncate">{song.title}</div>
-                <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
+            <p className="font-medium truncate">{song.title}</p>
+            <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
           </div>
+          <PlayButton song={song} />
         </div>
-        // TODO: Add play button
       ))}
     </div>
   );
 };
-
 export default FeaturedSection;
