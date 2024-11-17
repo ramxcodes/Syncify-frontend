f6f0a608e1174ff79cd9378f4891df9766d90fc2
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-17 17:33:11 +0530

    Created Admin Route 🛣️

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
 import { AlbumPage } from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           }
         />
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
 
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
