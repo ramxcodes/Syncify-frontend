d1b4eb75868fd0437ad1d90307a9e4e083a3c498
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-17 18:01:21 +0530

    Added Toaster 🍪

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MainLayout from "./layout/MainLayout";
 import ChatPage from "./pages/chat/ChatPage";
 import { AlbumPage } from "./pages/album/AlbumPage";
 import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/albums/:albumId" element={<AlbumPage />} />
         </Route>
       </Routes>
+      <Toaster />
     </>
   );
 }
