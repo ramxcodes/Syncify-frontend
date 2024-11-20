45f280b5e13c85f3b79c68be62070b66abb27a4f
Ramkrishna Swarnkar <128958601+ramxcodes@users.noreply.github.com>
2024-11-20 19:59:50 +0530

    Album page Responsive now 💅🏻

--- src/pages/album/AlbumPage.tsx
+++ src/pages/album/AlbumPage.tsx
@@ -50,24 +50,24 @@ export const AlbumPage = () => {
           {/* bg gradient */}
           <div
             className="absolute inset-0 bg-gradient-to-b from-[#5038a0]/80 via-zinc-900/80
-					 to-zinc-900 pointer-events-none"
+           to-zinc-900 pointer-events-none"
             aria-hidden="true"
           />
 
           {/* Content */}
           <div className="relative z-10">
-            <div className="flex p-6 gap-6 pb-8">
+            <div className="flex flex-col md:flex-row p-6 gap-6 pb-8">
               <img
                 src={currentAlbum?.imageUrl}
                 alt={currentAlbum?.title}
-                className="w-[240px] h-[240px] shadow-xl rounded"
+                className="w-48 h-48 md:w-[240px] md:h-[240px] shadow-xl rounded mx-auto md:mx-0"
               />
-              <div className="flex flex-col justify-end">
+              <div className="flex flex-col justify-end text-center md:text-left">
                 <p className="text-sm font-medium">Album</p>
-                <h1 className="text-7xl font-bold my-4">
+                <h1 className="text-4xl md:text-7xl font-bold my-4">
                   {currentAlbum?.title}
                 </h1>
-                <div className="flex items-center gap-2 text-sm text-zinc-100">
+                <div className="flex flex-wrap justify-center md:justify-start items-center gap-2 text-sm text-zinc-100">
                   <span className="font-medium text-white">
                     {currentAlbum?.artist}
                   </span>
@@ -77,13 +77,13 @@ export const AlbumPage = () => {
               </div>
             </div>
 
-            {/* play button */}
-            <div className="px-6 pb-4 flex items-center gap-6">
+            {/* Play Button */}
+            <div className="px-6 pb-4 flex justify-center md:justify-start items-center gap-6">
               <Button
                 onClick={handlePlayAlbum}
                 size="icon"
                 className="w-14 h-14 rounded-full bg-green-500 hover:bg-green-400 
-                hover:scale-110 transition-all"
+              hover:scale-110 transition-all"
               >
                 {isPlaying &&
                 currentAlbum?.songs.some(
@@ -99,7 +99,7 @@ export const AlbumPage = () => {
             {/* Table Section */}
             <div className="bg-black/20 backdrop-blur-sm">
               {/* Table Header */}
-              <div className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm">
+              <div className="hidden md:grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm">
                 <div>#</div>
                 <div>Title</div>
                 <div>Released Year</div>
@@ -118,10 +118,10 @@ export const AlbumPage = () => {
                       <div
                         onClick={() => handlePlaySong(index)}
                         key={song._id}
-                        className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
-                      text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer "
+                        className="grid grid-cols-1 md:grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm 
+                      text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer"
                       >
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-left">
                           {isCurrentSong && isPlaying ? (
                             <div className="size-4 text-green-500">♫</div>
                           ) : (
@@ -138,20 +138,22 @@ export const AlbumPage = () => {
                           <img
                             src={song.imageUrl}
                             alt={song.title}
-                            className="size-10"
+                            className="size-10 w-10 h-10 md:w-12 md:h-12 rounded"
                           />
 
                           <div>
                             <div className={`font-medium text-white`}>
                               {song.title}
                             </div>
-                            <div>{song.artist}</div>
+                            <div className="text-xs md:text-sm">
+                              {song.artist}
+                            </div>
                           </div>
                         </div>
-                        <div className="flex items-center">
+                        <div className="hidden md:flex items-center">
                           {song.createdAt.split("T")[0]}
                         </div>
-                        <div className="flex items-center">
+                        <div className="hidden md:flex items-center">
                           {formatDuration(song.duration)}
                         </div>
                       </div>
